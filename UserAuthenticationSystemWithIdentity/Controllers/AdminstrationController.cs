85cd92340acb51ff7b0648f311115d8ff12bcdf9
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-07 00:22:22 +0500

    Add project files.

---
+++ UserAuthenticationSystemWithIdentity/Controllers/AdminstrationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthenticationSystemWithIdentity.Controllers
+{
+    public class AdminstrationController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminstrationController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
